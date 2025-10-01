4341e413db885af75b65159fc9ab1873cf9d3f90
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-01 17:22:03 +1000

    copy sorted shuffle arrays

---
+++ copySorted.js
@@ -0,0 +1,29 @@
+/**
+ * Copy and sort array
+ * importance: 5
+ * We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
+ * Create a function copySorted(arr) that returns such a copy.
+ * let arr = ["HTML", "JavaScript", "CSS"];
+ * let sorted = copySorted(arr);
+ * alert( sorted ); // CSS, HTML, JavaScript
+ * alert( arr ); // HTML, JavaScript, CSS (no changes)
+ */
+
+function copySorted(arr)
+{
+    return arr
+        .toSorted()
+}
+
+/**
+ * Alternative published solution
+ * function copySorted(arr)
+ * {
+ * return arr.slice().sort();
+ * }
+ * 
+ * let arr = ["HTML", "JavaScript", "CSS"];
+ * let sorted = copySorted(arr);
+ * alert( sorted );
+ * alert( arr );
+ */
---
+++ filterUnique.js
@@ -0,0 +1,22 @@
+/**
+ * Filter unique array members
+ * importance: 4
+ * Let arr be an array.
+ * Create a function unique(arr) that should return an array with unique items of arr.
+ * For instance:
+ * function unique(arr) {
+ * // your code
+ * }
+ * let strings = ["Hare", "Krishna", "Hare", "Krishna",
+ * "Krishna", "Krishna", "Hare", "Hare", ":-O"
+ * ];
+ * alert( unique(strings) ); // Hare, Krishna, :-O
+ */
+
+function unique(arr)
+{
+    return arr
+        .map((element, index) => {
+            if (element )
+        })
+}
